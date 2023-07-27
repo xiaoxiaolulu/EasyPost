9ee8986743f55c74a0f4067f9911fb9f87a629b1
“null.lu” <null.lu@gtechdigi.com>
2023-07-27 18:05:52 +0800

    add pytest conftest module

--- config/settings.py
+++ config/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
