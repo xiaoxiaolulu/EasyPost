d432b8d94ebae40fa6d38597917983496f514002
“null.lu” <null.lu@gtechdigi.com>
2023-08-01 18:08:47 +0800

    add api models1

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,26 +1,26 @@
 // 引入windi css
-import 'web/src/plugins/windi.css'
+import '@/plugins/windi.css'
 
 // 导入全局的svg图标
-import 'web/src/plugins/svgIcon'
+import '@/plugins/svgIcon'
 
 // 初始化多语言
-import { setupI18n } from 'web/src/plugins/vueI18n'
+import { setupI18n } from '@/plugins/vueI18n'
 
 // 引入状态管理
-import { setupStore } from 'web/src/store'
+import { setupStore } from '@/store'
 
 // 全局组件
-import { setupGlobCom } from 'web/src/components'
+import { setupGlobCom } from '@/components'
 
 // 引入element-plus
-import { setupElementPlus } from 'web/src/plugins/elementPlus'
+import { setupElementPlus } from '@/plugins/elementPlus'
 
 // 引入全局样式
-import 'web/src/styles/index.less'
+import '@/styles/index.less'
 
 // 引入动画
-import 'web/src/plugins/animate.css'
+import '@/plugins/animate.css'
 
 // 路由
 import { setupRouter } from './router'
