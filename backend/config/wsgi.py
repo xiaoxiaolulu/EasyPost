51a02c3a3111a7cdca16a0522f2dce9b3cf8d506
“null.lu” <null.lu@gtechdigi.com>
2023-08-05 00:24:10 +0800

    :bookmark: 绵绵若存，用之不勤。

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
