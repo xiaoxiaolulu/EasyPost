39640d0cf87f65075706ec035580735c3c5b0617
“null.lu” <null.lu@gtechdigi.com>
2023-08-05 21:10:22 +0800

    :bookmark: 人生百年，同归于尽；

--- web/src/views/https/components/RequestParser.ts
+++ web/src/views/https/components/RequestParser.ts
@@ -1,50 +1,33 @@
-
-
 export const requestParser = (body: any) => {
     let req = {}
-    for(let key in body) {
-        if(key === "request4"){
-            req["method"] = body[key]
-        }
-        if(key === "addr"){
-            req["url"] = body[key]
-        }
-        if(key === "queryParams" && body[key][0].name.length != 0){
-            let arrParams = [];
-            let _queryParamsStr = '';
-            for (let i = 0; i < body[key].length; i++) {
-                try {
-                    let queryParamKey = body[key][i]['name'];
-                    let queryParamVal = body[key][i]['value']
-                    arrParams.push(queryParamKey + '=' + queryParamVal);
-                } catch (e) {
-                    console.log(e)
+    for (let key in body) {
+        switch (key) {
+            case "request4":
+                req["method"] = body[key]
+                break
+            case "addr":
+                req["url"] = body[key]
+                break
+            case "queryParams":
+                if (body[key][0].name.length != 0) {
+                    req["params"] = body[key].map((param: any) => `${param.name}=${param.value}`).join("&")
                 }
-            }
-            let getParams = (arrParams).join("&");
-            _queryParamsStr += getParams;
-            req["params"] = _queryParamsStr
-        }
-        if (key === "head" && body[key][0].name.length != 0) {
-            let _headers = {};
-            for (let i = 0; i < body[key].length; i++) {
-                let headerKey = body[key][i]["name"];
-                _headers[headerKey] = body[key][i]["value"];
-            }
-            req["headers"] = _headers
-        }
-        if (key === "formParams" && body[key][0].name.length != 0){
-            let _requestFormData = {};
-            if (body[key].length != 0) {
-                for (let i = 0; i < body[key].length; i++) {
-                    let requestFormDataKey = body[key][i]["name"];
-                    _requestFormData[requestFormDataKey] = body[key][i]["value"];
+                break
+            case "head":
+                if (body[key][0].name.length != 0) {
+                    req["headers"] = body[key].reduce((headers: any, header: any) => ({...headers, [header.name]: header.value}), {})
                 }
-            }
-            req["data"] = _requestFormData
-        }
-        if (key === 'codeContent' && body["codeContent"].length != 0) {
-            req["json"] = JSON.parse(body[key])
+                break
+            case "formParams":
+                if (body[key][0].name.length != 0) {
+                    req["data"] = body[key].reduce((formData: any, formParam: any) => ({...formData, [formParam.name]: formParam.value}), {})
+                }
+                break
+            case 'codeContent':
+                if (body["codeContent"].length != 0) {
+                    req["json"] = JSON.parse(body[key])
+                }
+                break
         }
     }
     return req
