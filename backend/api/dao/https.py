4f803f451e769a5f980bcac4e9982393fe897553
“null.lu” <null.lu@gtechdigi.com>
2023-09-13 15:16:57 +0800

    123

--- backend/api/dao/https.py
+++ backend/api/dao/https.py
@@ -1,5 +1,13 @@
+from api.models.https import Relation
 
 
 class HttpDao:
 
-    pass
+    @staticmethod
+    def get_directory_tree(project_id: int):
+        try:
+            tree = Relation.objects.get(project__id=project_id)
+            tree = eval(tree.tree)
+            return tree
+        except (Relation.DoesNotExist, Exception):
+            raise Exception("获取目录详情失败")
