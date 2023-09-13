46c4068b52bd1ac9a4a93207634bc13e651807c4
“null.lu” <null.lu@gtechdigi.com>
2023-09-13 15:38:10 +0800

    123

--- backend/api/dao/https.py
+++ backend/api/dao/https.py
@@ -1,4 +1,5 @@
-from api.models.https import Relation
+from api.models.https import Relation, Api
+from utils.trees import collections_directory_id, get_relation_tree
 
 
 class HttpDao:
@@ -11,3 +12,23 @@ class HttpDao:
             return tree
         except (Relation.DoesNotExist, Exception):
             raise Exception("获取目录详情失败")
+
+    @classmethod
+    def list_test_case(cls, queryset, node: int, project_id: int, name: str = ""):
+        try:
+            if project_id:
+
+                if node == 1:
+                    queryset = queryset
+                if node != 1:
+                    tree = cls.get_directory_tree(project_id)
+                    children_tree = get_relation_tree(tree, node)
+                    directory_ids = collections_directory_id(children_tree, node)
+                    queryset.filter(project__id=project_id, directory_id__in=directory_ids)
+                if name:
+                    queryset.queryset.filter(name=name)
+
+                return queryset
+
+        except (Api.DoesNotExist, Exception):
+            raise Exception("获取测试用例失败")
