58fce8a647db134773aade4ec4c6897cfc2ea88d
“null.lu” <null.lu@gtechdigi.com>
2023-12-07 10:44:54 +0800

    降低代码复杂度

--- backend/api/dao/user.py
+++ backend/api/dao/user.py
@@ -19,31 +19,40 @@ REGEX_EMAIL = "/^[a-zA-Z0-9_\.]+@[a-zA-Z0-9-]+[\.a-zA-Z]+$/" # noqa
 class UserDao:
 
     @staticmethod
-    def register_user_validate(account: str, account_type: str) -> None:
-        """
-        注册用户验证
-        :param account:  注册账号, str object.
-        :param account_type: 账号类型, str object.
-        :return: None
-        """
-        try:
-            user = User.objects.filter(Q(mobile=account) | Q(email=account)).first()
+    def validate_account_type(account: str, account_type: str) -> None:
+        """验证账号类型的函数"""
+        if account_type == 'mobile':
+            if not re.match(REGEX_MOBILE, account):
+                raise Exception("手机号码不合法!")
+        elif account_type == "email":
+            if not re.match(REGEX_EMAIL, account):
+                raise Exception("邮箱账号不合法!")
+        else:
+            raise Exception("账号类型不合法!")
 
+    @staticmethod
+    def check_user_existence(account: str) -> None:
+        """检查用户是否存在的函数"""
+        try:
+            user = User.objects.get(mobile=account) or User.objects.get(email=account)
             if user:
                 raise Exception("用户名或邮箱已存在!")
+        except User.DoesNotExist:
+            pass
 
-            if account_type == 'mobile':
-                if not re.match(REGEX_MOBILE, account):
-                    raise Exception("手机号码不合法!")
+    @staticmethod
+    def check_verify_code_time(account: str) -> None:
+        """检查验证码发送时间的函数"""
+        one_minutes_ago = datetime.now() - timedelta(minutes=1)
+        if VerifyCode.objects.filter(add_time__gt=one_minutes_ago, account=account).exists():
+            raise Exception("距离上一次发送未超过60s")
 
-            if account_type == "email":
-                if not re.match(REGEX_EMAIL, account):
-                    raise Exception("邮箱账号不合法!")
-
-            one_minutes_ago = datetime.now() - timedelta(hours=0, minutes=1, seconds=0)
-            if VerifyCode.objects.filter(add_time__gt=one_minutes_ago, account=account).count():
-                raise Exception("距离上一次发送未超过60s")
-
+    def register_user_validate(self, account: str, account_type: str) -> None:
+        """注册用户验证的主函数"""
+        try:
+            self.validate_account_type(account, account_type)
+            self.check_user_existence(account)
+            self.check_verify_code_time(account)
         except Exception as err:
             raise Exception(f"用户获取验证码失败 -> {err}")
 
