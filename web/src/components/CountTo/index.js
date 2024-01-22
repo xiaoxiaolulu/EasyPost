79316c112087a23053f726f24d68503dc130414d
Null <546464268@qq.com>
2024-01-22 18:07:06 +0800

    Delete web directory

--- web/src/components/CountTo/index.js
+++
@@ -1,5 +0,0 @@
-import CountTo from './index.vue'
-export default CountTo
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.component('count-to', CountTo)
-}
--- web/src/config/index.ts
+++
@@ -1,2 +0,0 @@
-// * 默认主题颜色
-export const PRIMARY_COLOR: string = "#409eff";
