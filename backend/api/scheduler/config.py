404f93b8f3f11b4e190625d10d0d1623f35a271b
xiaoxialulu <546464268@qq.com>
2024-05-21 11:23:20 +0800

    Update scheduler config and fix enum error.

--- backend/api/scheduler/config.py
+++ backend/api/scheduler/config.py
@@ -1,10 +1,15 @@
 from api.scheduler.sqlalchemy_store import SQLAlchemyJobStore
-
+from config.settings import DATABASES as DB
 
 config = {
     'apscheduler.jobstores': {
-        'default': SQLAlchemyJobStore(url='mysql://root:123456@127.0.0.1:3306/easypost?charset=utf8')
-    },
+        'default': SQLAlchemyJobStore(
+            url=f'mysql://{DB["default"]["USER"]}:'
+                f'{DB["default"]["PASSWORD"]}@'
+                f'{DB["default"]["HOST"]}:'
+                f'{DB["default"]["PORT"]}/'
+                f'{DB["default"]["NAME"]}?charset=utf8'
+        )},
     # 执行器配置 使用线程池执行器，最大10个线程
     'apscheduler.executors.default': {
         'class': 'apscheduler.executors.pool:ThreadPoolExecutor',
